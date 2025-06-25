d759529e4aa9ba38cc998b6858c0cbb9c3c08cf2
danyk <danykdev@gmail.com>
2025-06-25 11:20:24 +0300

    add datatables and pagination

--- resources/js/categories.js
+++ resources/js/categories.js
@@ -1,5 +1,83 @@
 import { Modal } from 'bootstrap'
 import { get, post, del } from './ajax'
+import DataTable from 'datatables.net'
+
+window.addEventListener('DOMContentLoaded', function () {
+    const editCategoryModal = new Modal(document.getElementById('editCategoryModal'))
+
+    const table = new DataTable('#categoriesTable', {
+        serverSide: true,
+        ajax: '/categories/load',
+        orderMulti: false,
+        columns: [
+            { data: 'name' },
+            { data: 'createdAt' },
+            { data: 'updatedAt' },
+            {
+                sortable: false,
+                data: row => `
+                    <div class="d-flex flex-">
+                        <button type="submit" class="btn btn-outline-primary delete-category-btn" data-id="${row.id}">
+                            <i class="bi bi-trash3-fill"></i>
+                        </button>
+                        <button class="ms-2 btn btn-outline-primary edit-category-btn" data-id="${row.id}">
+                            <i class="bi bi-pencil-fill"></i>
+                        </button>
+                    </div>
+                `,
+            },
+        ],
+    })
+
+    document.querySelector('#categoriesTable').addEventListener('click', function (event) {
+        const editBtn = event.target.closest('.edit-category-btn')
+        const deleteBtn = event.target.closest('.delete-category-btn')
+
+        if (editBtn) {
+            const categoryId = editBtn.getAttribute('data-id')
+
+            get(`/categories/${categoryId}`).
+            then(response => response.json()).
+            then(response => openEditCategoryModal(editCategoryModal, response))
+        } else {
+            const categoryId = deleteBtn.getAttribute('data-id')
+
+            if (confirm('Are you sure you want to delete this category?')) {
+                del(`/categories/${categoryId}`).then(() => {
+                    table.draw()
+                })
+            }
+        }
+    })
+
+    document.querySelector('.save-category-btn').addEventListener('click', function (event) {
+        const categoryId = event.currentTarget.getAttribute('data-id')
+
+        post(`/categories/${categoryId}`, {
+            name: editCategoryModal._element.querySelector('input[name="name"]').value,
+        }, editCategoryModal._element).then(response => {
+            if (response.ok) {
+                table.draw()
+                editCategoryModal.hide()
+            }
+        })
+    })
+})
+
+function openEditCategoryModal (modal, { id, name }) {
+    const nameInput = modal._element.querySelector('input[name="name"]')
+
+    nameInput.value = name
+
+    modal._element.querySelector('.save-category-btn').setAttribute('data-id', id)
+
+    modal.show()
+}
+
+/*
+import { Modal } from 'bootstrap'
+import { get, post, del } from './ajax'
+import DataTable from 'datatables.net'
 
 window.addEventListener('DOMContentLoaded', function () {
     const editCategoryModal = new Modal(document.getElementById('editCategoryModal'))
@@ -12,9 +90,9 @@ window.addEventListener('DOMContentLoaded', function () {
             then(response => response.json()).
             then(response => openEditCategoryModal(editCategoryModal, response))
 
-            /*fetch(`/categories/${categoryId}`).then(res => res.json()).then(res => {
+            /!*fetch(`/categories/${categoryId}`).then(res => res.json()).then(res => {
                 openEditCategoryModal(editCategoryModal, res)
-            })*/
+            })*!/
         })
     })
 
@@ -31,7 +109,7 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         })
 
-        /*fetch(`/categories/${categoryId}`, {
+        /!*fetch(`/categories/${categoryId}`, {
             method: 'POST',
             body: JSON.stringify({
                 name: editCategoryModal._element.querySelector('input[name="name"]').value,
@@ -43,7 +121,7 @@ window.addEventListener('DOMContentLoaded', function () {
                 'Content-Type': 'application/json',
                 'X-Requested-With': 'XMLHttpRequest',
             },
-        }).then(res => console.log(res))*/
+        }).then(res => console.log(res))*!/
     })
 
     document.querySelectorAll('.delete-category-btn').forEach(button => {
@@ -55,7 +133,7 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         })
     })
-    
+
 })
 
 function getCsrfFields () {
@@ -82,3 +160,4 @@ function openEditCategoryModal (modal, { id, name }) {
 
     modal.show()
 }
+*/
